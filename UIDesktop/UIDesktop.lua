9298996b2d37b40b319b383f79ab586885893fd9
minh-p <minhisverynormal@gmail.com>
2021-10-31 15:07:42 -0700

    separation: border focus signal

--- UIDesktop/UIDesktop.lua
+++ UIDesktop/UIDesktop.lua
@@ -1,3 +1,6 @@
 -- Purpose: awesomewm desktopUI - layouts, statusbar
 require("UIDesktop.wibar")
 require("UIDesktop.titlebar")
+
+client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
